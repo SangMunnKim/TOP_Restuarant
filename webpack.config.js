7083761891ca8dcb4c1356c66569c14c884ea71f
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-07-18 11:16:02 +0200

    webpack configuration
    - added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins : [
+    new HTMLWebpackPlugin({
+        title: 'main.[contenthash].js',
+        template: 'src/index.html',
+    }),
+  ], 
   output: {
-    filename: 'main.js',
+    filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
